4bf760b892e965c92d37bbc234d20e227768df8b
yxk <441732552@qq.com>
2021-01-13 00:00:31 +0800

    test1 开发购物车第二天

--- comment/src/main/java/org/imooc/controller/api/CartController.java
+++ comment/src/main/java/org/imooc/controller/api/CartController.java
@@ -4,6 +4,10 @@ package org.imooc.controller.api;
 public class CartController {
 
     public void addCart(){
-        System.out.println("test 开发购物车第一天");
+        System.out.println("test1 开发购物车第一天");
+    }
+
+    public void updateCart(){
+        System.out.println("test1 开发购物车第二天");
     }
 }
